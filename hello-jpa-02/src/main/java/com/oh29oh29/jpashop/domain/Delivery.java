2d31941490c13f75b2d47cf4143dfa9236600283
oh29oh29 <oh29oh29.develop@gmail.com>
2022-01-12 22:15:30 +0900

    JPA 시작하기 - shop
    다양한 연관관계 매핑
    - ManyToMany 는 실제 사용 X
    - N:M 관계는 중간 테이블을 이용해서 1:N, N:1 로 만들기

---
+++ hello-jpa-02/src/main/java/com/oh29oh29/jpashop/domain/Delivery.java
@@ -0,0 +1,18 @@
+package com.oh29oh29.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+}
---
+++ hello-jpa-02/src/main/java/com/oh29oh29/jpashop/domain/DeliveryStatus.java
@@ -0,0 +1,4 @@
+package com.oh29oh29.jpashop.domain;
+
+public enum DeliveryStatus {
+}
