be7514082cfbb2643fd3cdcd64d5a825d5b23561
oh29oh29 <oh29oh29.develop@gmail.com>
2022-04-09 10:46:37 +0900

    [Hello Spring JPA 001] 상품 도메인 개발

--- hello-spring-jpa-001/src/main/java/com/oh29oh29/domain/item/Item.java
+++ hello-spring-jpa-001/src/main/java/com/oh29oh29/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.oh29oh29.domain.item;
 
 import com.oh29oh29.domain.Category;
+import com.oh29oh29.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * stock 증가
+     * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * */
+    public void removeStock(int quantity) {
+        int restTock = this.stockQuantity - quantity;
+        if (restTock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restTock;
+    }
 }
