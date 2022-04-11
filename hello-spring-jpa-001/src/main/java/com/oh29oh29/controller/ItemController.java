e93e7ebc2bb3f39b152f9dc94045e2f5850ec249
oh29oh29 <oh29oh29.develop@gmail.com>
2022-04-11 22:04:34 +0900

    [Hello Spring JPA 001] 상품 등록

---
+++ hello-spring-jpa-001/src/main/java/com/oh29oh29/controller/ItemController.java
@@ -0,0 +1,35 @@
+package com.oh29oh29.controller;
+
+import com.oh29oh29.domain.item.Book;
+import com.oh29oh29.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        final Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
