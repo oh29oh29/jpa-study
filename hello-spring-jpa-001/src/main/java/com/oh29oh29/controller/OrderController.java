4259d6831f2fa388c8f27848258cdd5fd302fa59
oh29oh29 <oh29oh29.develop@gmail.com>
2022-04-11 22:48:42 +0900

    [Hello Spring JPA 001] 상품 주문

---
+++ hello-spring-jpa-001/src/main/java/com/oh29oh29/controller/OrderController.java
@@ -0,0 +1,45 @@
+package com.oh29oh29.controller;
+
+import com.oh29oh29.domain.Member;
+import com.oh29oh29.domain.item.Item;
+import com.oh29oh29.service.ItemService;
+import com.oh29oh29.service.MemberService;
+import com.oh29oh29.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        final List<Member> members = memberService.findMembers();
+        final List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(
+            @RequestParam("memberId") Long memberId,
+            @RequestParam("itemId") Long itemId,
+            @RequestParam("count") int count
+    ) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
